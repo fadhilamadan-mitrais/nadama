1caca1e784f758146eefcf5691f53a11edee5480
Fadhil Amadan <fadhil.amadan@mitrais.com>
2021-12-22 10:04:17 +0700

    refactor(page): add new ui landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,13 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
       </main>
     </div>
   )
