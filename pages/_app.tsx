0e44904666fb8b4c6cbc51ec065833dd037101cd
Fadhil Amadan <fadhil.amadan@mitrais.com>
2021-12-24 10:03:45 +0700

    refactor(app): add new struct with ant design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'antd/dist/antd.css';
+import '~styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import LayoutFooter from '~components/layouts/footer';
+import LayoutNavbar from '~components/layouts/navbar';
 
-export default MyApp
+import { Layout } from 'antd';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+const { Header, Footer, Content } = Layout;
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Layout>
+      <Header>
+        <Head>
+          <title>Nadama</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <LayoutNavbar />
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div
+          style={{
+            minHeight: '280px',
+            padding: '24px',
+            background: '#fff',
+          }}
+        >
+          <Component {...pageProps} />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        <LayoutFooter />
+      </Footer>
+    </Layout>
+  );
+};
+
+export default MyApp;
